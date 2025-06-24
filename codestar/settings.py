ab5a129355b1d36431f207d9f627820fc2173a59
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-06-24 18:31:03 +0100

    deploy project

--- codestar/settings.py
+++ codestar/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-f(b%7#w0@e*dr%tdgn6e^23(!5(yh=y-gue(ij(x-^v#8h(i4k
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com',]
+ALLOWED_HOSTS = ['.herokuapp.com','127.0.0.1',]
 
 
 # Application definition
