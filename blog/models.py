83b5254c8c7d4fa132f556887bc913daaf082f23
AugustinaGonja <qqwqdddj@students.codeinstitute.net>
2025-06-26 20:27:44 +0100

    install gunicorn

--- blog/models.py
+++ blog/models.py
@@ -15,6 +15,8 @@ class Post(models.Model):
     status = models.IntegerField(choices=STATUS, default=0)
     excerpt =  models.TextField(blank=True)
     updated_on = models.DateTimeField(auto_now=True)
+    class Meta:
+        ordering = ["-created_on"]
 
     
 class Comment(models.Model):
@@ -25,3 +27,4 @@ class Comment(models.Model):
     body = models.TextField()
     approved = models.BooleanField(default=False)
     created_on = models.DateTimeField(auto_now_add=True)
+    
